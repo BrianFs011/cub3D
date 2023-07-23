44fa2e3d42f906ab6c04e5bbf41340bfc5788182
briferre <briferre@student.42sp.org.br>
2023-07-23 03:27:55 -0400

    add find personage que procura o personagem no mapa

--- src/map_load.c
+++ src/map_load.c
@@ -6,7 +6,7 @@
 /*   By: briferre <briferre@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/23 08:27:09 by briferre          #+#    #+#             */
-/*   Updated: 2023/07/17 13:40:32 by briferre         ###   ########.fr       */
+/*   Updated: 2023/07/23 03:26:50 by briferre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,30 @@ int	map_size(int fd)
 	return (i);
 }
 
+t_points	find_personage(char *string, t_points cp, int i)
+{
+	t_points	p;
+	int			j;
+
+	j = -1;
+	p.x = cp.x;
+	p.y = cp.y;
+	while (string[++j])
+	{
+		if (string[j] == 'N'
+			|| string[j] == 'S'
+			|| string[j] == 'E'
+			|| string[j] == 'W')
+		{
+			p.x = (double)i + 0.5;
+			p.y = (double)j + 0.5;
+			printf("%lf %lf %d %d\n", p.x, p.y, i, j);
+			return (p);
+		}
+	}
+	return (p);
+}
+
 void	get_map(t_mlx *mlx, int fd)
 {
 	int		i;
@@ -36,22 +60,24 @@ void	get_map(t_mlx *mlx, int fd)
 
 	mlx->map_size = map_size(fd);
 	mlx->map = malloc(sizeof(char *) * mlx->map_size + sizeof(char *));
-	i = mlx->map_size + 1;
-	mlx->map[--i] = NULL;
+	i = -1;
 	string = get_next_line(fd);
+	mlx->camera.position = find_personage(string, mlx->camera.position, i);
 	while (string)
 	{
-		mlx->map[--i] = string;
+		mlx->map[++i] = string;
+		mlx->camera.position = find_personage(string, mlx->camera.position, i);
 		string = get_next_line(fd);
 	}
+	mlx->map[++i] = NULL;
 }
 
-void	map_load(t_mlx *mlx)
+void	map_load(t_mlx *mlx, char **argv)
 {
 	int	fd;
 
 	(void)mlx;
-	fd = open("maps/map1.cub", O_RDONLY);
+	fd = open(argv[1], O_RDONLY);
 	get_map(mlx, fd);
 	close(fd);
 }
