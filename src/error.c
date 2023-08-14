ef1b09f581518bae96f7e64dfab25398a4dfb09c
briferre <briferre@student.42sp.org.br>
2023-08-14 16:30:22 -0400

    add mensagem Erro\n

--- src/error.c
+++ src/error.c
@@ -6,7 +6,7 @@
 /*   By: briferre <briferre@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 15:57:32 by briferre          #+#    #+#             */
-/*   Updated: 2023/08/14 16:02:36 by briferre         ###   ########.fr       */
+/*   Updated: 2023/08/14 16:28:52 by briferre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 void	set_error(t_error *error, char *msg)
 {
+	printf("Error\n");
 	error->type = 1;
 	error->error_message = msg;
 }
